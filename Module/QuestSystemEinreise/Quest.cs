1c4339d10bdd79e16b3b8177ad92d1eee93d6248
Quixixd <90565445+Quixixd@users.noreply.github.com>
2022-08-03 15:36:51 +0200

    @Nicergrossa123 finished 40 jahre die flippers

--- Module/QuestSystemEinreise/Quest.cs
+++ Module/QuestSystemEinreise/Quest.cs
@@ -3,20 +3,42 @@ using System.Collections.Generic;
 using System.Text;
 using GTANetworkAPI;
 using Nexus.Module;
+using Nexus.Module.Items;
+using Nexus.Module.Logging;
 using Nexus.Module.Menu;
+using Nexus.Module.Players;
+using Nexus.Module.Players.Db;
 
 namespace nexusrp.Module.QuestSystemEinreise
 {
     public class QuestSystemEinreise : Module<QuestSystemEinreise>
     {
-        public static Vector3 PositionPC1 = new Vector3(-1077.96, -2810.92, 27.7087);
-        public static Vector3 PositionPC2 = new Vector3(-1067.82, -2811.16, 27.7087);
-        public static Vector3 PositionPC3 = new Vector3(-1073.29, -2820.76, 27.7087);
-        public static Vector3 PositionPC4 = new Vector3(-1083.56, -2820.45, 27.7087);
+        public static Vector3 GeilerTypderautosundgeldgibt = new Vector3(-218.09, -1163.06, 23.03);
 
         public override bool Load(bool reload = false)
         {
             return true;
+        }
+        public override bool OnKeyPressed(DbPlayer dbPlayer, Key key)
+        {
+            if (dbPlayer.Player.IsInVehicle) return false;
+            if (key == Key.E)
+            {
+                if (dbPlayer.Player.Position.DistanceTo(GeilerTypderautosundgeldgibt) < 1.5f && dbPlayer.HasData("QuestAn"))
+                {
+                    dbPlayer.Player.SendNotification("Hey, erstmal willkommen in Los Santos. Da du mich gefunden hast Habe ich ein Handy und 25k für dich!");
+                    dbPlayer.GiveMoney(25000);
+                    if (!dbPlayer.Container.AddItem(174, 1))
+                    {
+                        dbPlayer.SendNewNotification("Du hast keinen Platz fuer dieses Item!");
+                        return false;
+                    }
+
+                    dbPlayer.Container.AddItem(174, 1);
+                    dbPlayer.SetData("QuestAn", 0);
+                }
+            }
+            return false;
         }
     }
 }
